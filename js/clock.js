25d1b7a5ca57981865d48d3d3ecbe18755c6f8b7
Diana Tran <dianatran@Dianas-MacBook-Air.local>
2025-05-10 10:56:13 -0600

    Initial commit

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2023-10-29 12:00", "Asia/Kolkata");
+  let targetDate = moment.tz("2026-09-24 18:00", "Asia/Kolkata");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
